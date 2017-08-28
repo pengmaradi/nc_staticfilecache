f906fd26553aea4ffc6f07d3aac5e8aa8edf60a2
Christopher Kaster <kaster@ganzgraph.de>
2017-08-28 11:07:07 +0200

    fix web > info > static file cache not working, update em conf

---
+++ README.md
@@ -0,0 +1,3 @@
+# nc_staticfilecache
+
+A fork of [nc_staticfilecache](https://extensions.typo3.org/extension/nc_staticfilecache/) with improved TYPO3 8 LTS compatibility.
--- ext_emconf.php
+++ ext_emconf.php
@@ -14,8 +14,8 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'Static File Cache',
     'description' => 'Transparent static file cache solution using mod_rewrite and mod_expires. Increase response times for static pages by a factor of 230!!',
     'category' => 'fe',
-    'version' => '3.6.0',
-    'state' => 'stable',
+    'version' => '4.0.0',
+    'state' => 'beta',
     'modify_tables' => 'pages',
     'clearcacheonload' => true,
     'author' => 'Static File Cache team',
@@ -23,8 +23,8 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => 'Netcreators',
     'constraints' => [
         'depends' => [
-            'typo3' => '6.2.0-7.99.99',
-            'php' => '5.5.0-0.0.0',
+            'typo3' => '8.7.0-8.7.99',
+            'php' => '7.0.0-0.0.0',
         ],
     ],
     'autoload' => [
